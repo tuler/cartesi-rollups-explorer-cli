4581caba79a8f625746a18e834cf8e7f9a6e57e5
Danilo Tuler <tuler@pobox.com>
2025-07-08 16:57:53 -0400

    replacing meow with commander, and add tsup

--- src/cli.tsx
+++ src/cli.tsx
@@ -1,27 +1,14 @@
 #!/usr/bin/env node
+import { program } from "@commander-js/extra-typings";
 import { render } from "ink";
-import meow from "meow";
 import React from "react";
 import App from "./app.js";
 
-const cli = meow(
-    `
-	Usage
-	  $ cartesi-rollups-explorer-cli [service-url]
+program
+    .description("A CLI for Cartesi Rollups RPC")
+    .argument("[rpc-url]", "JSON-RPC endpoint", "http://127.0.0.1:6751/rpc")
+    .action((rpcUrl) => {
+        render(<App rpcUrl={rpcUrl} />);
+    });
 
-	Arguments
-		service-url  JSON-RPC endpoint (default: http://127.0.0.1:6751/rpc)
-
-	Examples
-	  $ cartesi-rollups-explorer-cli
-	  $ cartesi-rollups-explorer-cli http://127.0.0.1:6751/rpc
-`,
-    {
-        importMeta: import.meta,
-        flags: {},
-    },
-);
-
-const rpcUrl = cli.input[0] || "http://127.0.0.1:6751/rpc";
-
-render(<App rpcUrl={rpcUrl} />);
+program.parse();
